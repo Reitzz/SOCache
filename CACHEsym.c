f50082f69a6948a9e8acc23c8b97b5f83b4288fd
Reitzz <raul_carrascal@hotmail.com>
2020-11-28 05:50:13 -0800

    Añadido nombre + Inicializacion Variables

--- CACHEsym.c
+++ CACHEsym.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+typedef struct {
+ short int ETQ;
+ short int Datos[8];
+} T_LINEA_CACHE;
+
 int main() 
 { 
+    int i = 0, tiempoglobal = 0, numfallos = 0;;
+    T_LINEA_CACHE Cache;
+    Cache.ETQ = 0xFF;
+    for(i; i<=8; i++)
+        Cache.Datos[i] = 0;
     FILE* ram; 
     int display; 
     ram = fopen("RAM.bin", "rb"); // Hola dani
